b8474fe6824d7b70fb035b86530c98997f8ebddf
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-09-01 19:28:14 +0200

    basic working version

---
+++ index.js
@@ -0,0 +1,77 @@
+let X;
+let OPERATOR;
+let Y;
+let displayValue;
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(display) {
+  let result;
+  X = +X;
+  Y = +Y;
+  if (OPERATOR === "+") {
+    result = add(X, Y);
+  } else if (OPERATOR === "-") {
+    result = subtract(X, Y);
+  } else if (OPERATOR === "*") {
+    result = multiply(X, Y);
+  } else if (OPERATOR === "/") {
+    result = divide(X, Y);
+  }
+  display.textContent = result;
+  X = result;
+  Y = null;
+  OPERATOR = null;
+}
+
+const display = document.querySelector(".display");
+
+const digits = document.querySelectorAll(".digit");
+digits.forEach((digit) => {
+  digit.addEventListener("click", () => {
+    const value = digit.textContent;
+    if (OPERATOR) {
+      Y = Y ? Y + value : value;
+      display.textContent = Y;
+    } else {
+      X = X ? X + value : value;
+      display.textContent = X;
+    }
+    console.log(X, OPERATOR, Y);
+  });
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    const value = operator.textContent;
+    if (value === "=" && Y) {
+      operate(display);
+    } else {
+      OPERATOR = value;
+      display.textContent = null;
+    }
+  });
+});
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  X = null;
+  Y = null;
+  OPERATOR = null;
+  display.textContent = null;
+});
