aeb96291b066196d091db138c9835696b0360fe5
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-09-02 23:18:24 +0200

    idk

--- index.js
+++ index.js
@@ -1,17 +1,94 @@
-let X = "0";
-let Y = "";
+let LEFT_OPERAND = "0";
 let OPERATOR = "";
+let RIGHT_OPERAND = "";
+let CURRENT_OPERAND = "L"; // L or R
+let RESULT = "";
 
-// const DISPLAY = document.querySelector(".display");
-// let previousNum = "";
-// let currentNum = "";
-// let currentNum = document.querySelector(".display").textContent;
+const DISPLAY = document.querySelector(".display");
+const BUTTONS = document.querySelectorAll("button");
 
-const display = document.querySelector(".display");
-const clearButton = document.querySelector("button.clear");
-clearButton.addEventListener("click", () => {
+function handleOperand(operand, digit) {
+  let newOperand = "";
+  if (operand === "0" || !operand) {
+    newOperand = digit;
+  } else {
+    newOperand = operand + digit;
+  }
+  updateDisplay(newOperand);
+  return newOperand;
+}
+
+function handleDigit(button) {
+  console.log(LEFT_OPERAND, RESULT);
+  console.log("handleDigit");
+  const digit = button.textContent;
+  if (RESULT === LEFT_OPERAND) {
+    LEFT_OPERAND = "";
+  }
+  if (CURRENT_OPERAND === "L") {
+    LEFT_OPERAND = handleOperand(LEFT_OPERAND, digit);
+  } else {
+    RIGHT_OPERAND = handleOperand(RIGHT_OPERAND, digit);
+  }
+}
+
+function handleOperator(button) {
+  console.log("handleOperator");
+  OPERATOR = button.textContent;
+  //   if (LEFT_OPERAND && RIGHT_OPERAND) {
+  //     handleEquals();
+  //   }
+  //   if (RIGHT_OPERAND) {
+  //     RIGHT_OPERAND = "";
+  //   }
+  CURRENT_OPERAND = "R";
+}
+
+function handleDecimal() {}
+
+function handleBackspace() {}
+
+function handleClear() {
   init();
-});
+}
+
+function handleFunc() {}
+
+function operate(operation) {
+  const operationString = operation.toString();
+  LEFT_OPERAND = operationString;
+  RESULT = operationString;
+}
+
+function handleEquals() {
+  //   console.log("handleEquals");
+  //   console.log(LEFT_OPERAND, OPERATOR, RIGHT_OPERAND);
+  CURRENT_OPERAND = "L";
+  if (!RIGHT_OPERAND) {
+    RIGHT_OPERAND = LEFT_OPERAND;
+  }
+  const x = +LEFT_OPERAND;
+  const y = +RIGHT_OPERAND;
+  switch (OPERATOR) {
+    case "+": {
+      operate(x + y);
+      break;
+    }
+    case "-": {
+      operate(x - y);
+      break;
+    }
+    case "*": {
+      operate(x * y);
+      break;
+    }
+    case "/": {
+      operate(x / y);
+      break;
+    }
+  }
+  updateDisplay();
+}
 
 function prettifyNumber(n) {
   const parts = n.split(".");
@@ -33,56 +110,59 @@ function prettifyNumber(n) {
   }
 }
 
-function toggleNegative(n) {
-  function negativeToggle() {
-    if (!OPERATOR) {
-      if (Math.sign(+X) === 1) {
-        X = `${-Math.abs(+X)}`;
-      } else if (Math.sign(+X) === -1) {
-        X = `${Math.abs(+X)}`;
-      } else {
-        X = X;
-      }
-      display.textContent = X;
-    } else {
-      if (Math.sign(+Y) === 1) {
-        Y = `${-Math.abs(+Y)}`;
-      } else if (Math.sign(+Y) === -1) {
-        Y = `${Math.abs(+Y)}`;
-      } else {
-        Y = Y;
-      }
-      display.textContent = Y;
-    }
-  }
+function updateDisplay(operand = LEFT_OPERAND) {
+  console.log("updateDisplay:", operand);
+  DISPLAY.textContent = prettifyNumber(operand);
 }
 
-const digits = document.querySelectorAll(".digit");
-digits.forEach((digit) => {
-  digit.addEventListener("click", (e) => {
-    const input = e.target.textContent;
-    if (!OPERATOR) {
-      // sets first number (X)
-      if (input === "0" && X === "0") {
-        return;
-      }
-      if (X === "0") {
-        X = prettifyNumber(input);
-      } else {
-        X = prettifyNumber(X + input);
-      }
-      display.textContent = X;
-    } else {
-      // sets second number (Y)
-    }
-  });
-});
-
 function init() {
-  X = "0";
-  Y = "";
+  LEFT_OPERAND = "0";
+  RIGHT_OPERAND = "";
   OPERATOR = "";
-  display.textContent = X;
+  DISPLAY.textContent = LEFT_OPERAND;
 }
 
 init();
+
+BUTTONS.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const className = e.target.className;
+    // console.log("LEFT_OPERAND:", LEFT_OPERAND);
+    // console.log("RIGHT_OPERAND:", RIGHT_OPERAND);
+    // console.log("OPERATIR:", OPERATOR);
+    // console.log("=======================");
+    switch (className) {
+      case "digit": {
+        handleDigit(e.target);
+        break;
+      }
+      case "operator": {
+        handleOperator(e.target);
+        break;
+      }
+      case "decimal": {
+        handleDecimal(e.target);
+        break;
+      }
+      case "backspace": {
+        handleBackspace(e.target);
+        break;
+      }
+      case "clear": {
+        handleClear();
+        break;
+      }
+      case "func": {
+        handleFunc(e.target);
+        break;
+      }
+      case "equals": {
+        handleEquals(e.target);
+        break;
+      }
+    }
+    // if (className === "digit") {
+    //   handleDigit(e.target);
+    // } else if (className === "operator") {
+  });
+});
